1b99f6987ec2f464b1f347bfaed5c83f32b8a0ab
ruiming0114 <ruiming0114@buaa.edu.cn>
2020-10-26 10:06:28 +0800

    词法分析和语法分析

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -205,7 +205,6 @@ public final class Analyser {
         analyseConstantDeclaration();
         analyseVariableDeclaration();
         analyseStatementSequence();
-        throw new Error("Not implemented");
     }
 
     private void analyseConstantDeclaration() throws CompileError {
@@ -264,7 +263,7 @@ public final class Analyser {
 
             // 加入符号表，请填写名字和当前位置（报错用）
             String name = (String) nameToken.getValue();
-            addSymbol(name, false, false, nameToken.getStartPos());
+            addSymbol(name, initialized, false, nameToken.getStartPos());
 
             // 如果没有初始化的话在栈里推入一个初始值
             if (!initialized) {
